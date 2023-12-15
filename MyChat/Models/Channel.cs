91675e3d1ff51897b45beb321f92a0c486b321c2
Ville Ritala <ritalaville@gmail.com>
2023-12-15 10:58:16 +0200

    Test data updated

--- MyChat/Models/Channel.cs
+++ MyChat/Models/Channel.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Message> Messages { get; set; } = new List<Message>();
+        public List<Discussion> Discussions { get; set; } = new List<Discussion>();
     }
 }
--- MyChat/Services/ChannelService.cs
+++ MyChat/Services/ChannelService.cs
@@ -6,8 +6,69 @@ namespace MyChat.Services
     {
         private readonly List<Channel> _channels =
         [
-            new Channel { Id = 1, Name = "General" },
-            new Channel { Id = 2, Name = "Random" },
+            new Channel 
+            { 
+                Id = 1, Name = "General", 
+                Discussions  =
+                {
+                    new Discussion
+                    {
+                        Id = 1,
+                        Title = "Breaking news",
+                        ChannelId = 1,
+                        CreatedAt = DateTime.Now.AddHours(-1),
+                        Messages = new List<Message>
+                        {
+                            new Message { Id = 1, Content = "Hello!", Timestamp = DateTime.Now.AddMinutes(-30) },
+                            new Message { Id = 2, Content = "How are you?", Timestamp = DateTime.Now.AddMinutes(-20) }
+                        }
+                    },
+                    new Discussion
+                    {
+                        Id = 2,
+                        Title = "What is happening?",
+                        ChannelId = 2,
+                        CreatedAt = DateTime.Now.AddHours(-2),
+                        Messages = new List<Message>
+                        {
+                            new Message { Id = 3, Content = "Discussing important things.", Timestamp = DateTime.Now.AddMinutes(-40) },
+                            new Message { Id = 4, Content = "Any thoughts?", Timestamp = DateTime.Now.AddMinutes(-15) }
+                        }
+                    },
+                }
+            },
+            new Channel
+            {
+                Id = 1,
+                Name = "Politics",
+                Discussions =
+                {
+                    new Discussion
+                    {
+                        Id = 1,
+                        Title = "Laws",
+                        ChannelId = 1,
+                        CreatedAt = DateTime.Now.AddHours(-1),
+                        Messages = new List<Message>
+                        {
+                            new Message { Id = 1, Content = "Hello!", Timestamp = DateTime.Now.AddMinutes(-30) },
+                            new Message { Id = 2, Content = "How are you?", Timestamp = DateTime.Now.AddMinutes(-20) }
+                        }
+                    },
+                    new Discussion
+                    {
+                        Id = 2,
+                        Title = "NATO discussion",
+                        ChannelId = 2,
+                        CreatedAt = DateTime.Now.AddHours(-2),
+                        Messages = new List<Message>
+                        {
+                            new Message { Id = 3, Content = "Discussing important things.", Timestamp = DateTime.Now.AddMinutes(-40) },
+                            new Message { Id = 4, Content = "Any thoughts?", Timestamp = DateTime.Now.AddMinutes(-15) }
+                        }
+                    },
+                }
+            },
         ];
 
         public List<Channel> GetSerchedChannels(string searchTerm)
