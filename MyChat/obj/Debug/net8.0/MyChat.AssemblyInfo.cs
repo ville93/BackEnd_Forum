89c67a948719e1f9281571806e6c540a505bc4ce
Ville Ritala <ritalaville@gmail.com>
2024-03-01 13:19:18 +0200

    Readme update

--- MyChat/obj/Debug/net8.0/MyChat.AssemblyInfo.cs
+++ MyChat/obj/Debug/net8.0/MyChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1f31bc782fb0a947049d572dea257abaacbf5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98cdca3700a470090cb55932ebd52c2199a23668")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,14 @@
+# Back end for the forum
+
+This is a back end implementation for a discussion board application.
+
+Note! The implementation of the application is still in progress
+
+## The technologies used in the implementation
+
+C#
+.NET
+ASP.NET Core
+EF Core
+JSON Web Token (JWT) authentication
+xUnit
